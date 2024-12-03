c40bfe70661e1fe08f7ca177d762e2f48b5ef2e1
Roberto Velez <robertojvi@gmail.com>
2024-12-02 23:13:12 -0500

    Mejorado rutas App

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,19 @@ import Home from "./Routes/Home";
 import Contact from "./Routes/Contact";
 import Favs from "./Routes/Favs";
 import Detail from "./Routes/Detail";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
 import Layout from "./Layouts/Layout";
 
 const App = () => {
 	return (
-		<div className="App">
-			{/* Contenido principal cambia según la ruta */}
-			<div className="content">
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route path="/home" element={<Home />} />
-						<Route path="/contacto" element={<Contact />} />
-						<Route path="/favs" element={<Favs />} />
-						<Route path="/dentist/:id" element={<Detail />} />
-						<Route path="*" element={<Home />} /> {/* Ruta predeterminada */}
-					</Route>
-				</Routes>
-			</div>
-		</div>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route path="/home" element={<Home />} />
+				<Route path="/contacto" element={<Contact />} />
+				<Route path="/favs" element={<Favs />} />
+				<Route path="/dentist/:id" element={<Detail />} />
+				<Route path="*" element={<Home />} /> {/* Ruta predeterminada */}
+			</Route>
+		</Routes>
 	);
 };
 
